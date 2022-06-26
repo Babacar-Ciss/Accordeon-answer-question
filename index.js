fb8ce5f5c9c19f4c58987913c4968ab0373ad597
Babacar Ciss <kratosnvidia@gmail.com>
2022-06-26 00:36:23 +0000

    adding js file

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const questionComponent = document.getElementsByClassName("question");
+const [...question] = document.getElementsByClassName("question");
+const [...arrow] = document.getElementsByClassName("arrow");
+const [...answer] = document.getElementsByClassName("answer");
 
-console.log(questionComponent);
+let idQuestion = 0;
+
+//Selection Question's ID
+arrow.forEach((el, id) => {
+    el.addEventListener("click", () => {
+        
+                answer[id].classList.toggle("hidden"); 
+                question[id].classList.toggle("active");
+
+        idQuestion = id
+    })
+})
+
+
+// Function for displaying Answer base of ID
+function ShowAnswerById (id) {
+    
+}
